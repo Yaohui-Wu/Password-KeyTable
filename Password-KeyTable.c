50b62bba68b888ba7f18467ce3173cec5c892819
Yaohui-Wu <57257386+Yaohui-Wu@users.noreply.github.com>
2020-09-30 12:50:49 +0800

    first commit

--- Password-KeyTable.c
+++ Password-KeyTable.c
@@ -1,38 +1,22 @@
-// Usage (encryption): PasswordKeyTable plaintext.file cipher.file password
-// Usage (decryption): PasswordKeyTable cipher.file plaintext.file password
-// Compiled on MacOS, Linux and *BSD.
-// Talk is so easy, show you my GOD. WOW
+// Usage (encryption): PasswordKeyTable plaintext.file ciphertext.file password
+// Usage (decryption): PasswordKeyTable ciphertext.file plaintext.file password
+// Compiled on MacOS, Linux and *BSD in X86_64 platform.
+// Talk is so easy, show you my GOD.
+// Simple is beautiful.
 
+#include <fcntl.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    struct stat statFileSize;
-
-// get plaintext file size
-    stat(argv[1], &statFileSize);
-
-    unsigned long ulFileSize = statFileSize.st_size;
-
-    unsigned char *pucPlaintextOrCiphertext = (unsigned char*)malloc(ulFileSize);
-
-// open plaintext file descriptor
-    int iPlaintextFD = open(argv[1], O_RDONLY, S_IRUSR | S_IWUSR);
-
-// read data from plaintext file
-    read(iPlaintextFD, pucPlaintextOrCiphertext, ulFileSize);
-
-    close(iPlaintextFD);
-
     unsigned long ulPasswordLength;
 
 // You can set a password at least 8 or at most 16 characters.
 // You can freely to change to infinity as you wish. 
-// For example, 256 characters, 512 characters, 1024 characters, and so on until infinity. WOW!
+// For example, 256 characters, 512 characters, 1024 characters, and so on until infinity.
     unsigned char aucPassword[16];
 
 // To get the password, no more than 16 characters.
@@ -46,7 +30,7 @@ int main(int argc, char *argv[])
  * Key table of 256 values that you can set randomly,
  * yet you can freely to change to key table of 65536 values that you can set randomly,
  * you can also freely to change to key table of 4294967296 values that you can set randomly,
- * even if to change to key table of 18446744073709551616 values but limited to your machine memory. WOW!
+ * even if to change to key table of 18446744073709551616 values but limited to your machine memory.
 */
     unsigned char aucKeyTable[256] = {
           42, 32, 22, 12, 2, 19, 7, 47, 17, 5, 43, 37, 29, 13, 3, 41, 31, 23, 11, 0, 1, 4, 6, 8, 9, 10, 14, 15, 16, 18, 20, 21, 24, 25, 26, 27, 28, 30, 33, 34, 35, 36, 38, 39, 40, 44, 45, 46, 48, 49,
@@ -56,57 +40,70 @@ int main(int argc, char *argv[])
           252, 242, 232, 222, 212, 202, 239, 227, 251, 241, 233, 223, 211, 209, 200, 201, 203, 204, 205, 206, 207, 208, 210, 213, 214, 215, 216, 217, 218, 219, 220, 221, 224, 225, 226, 228, 229, 230, 231, 234, 235, 236, 237, 238, 240, 243, 244, 245, 246, 247, 248, 249, 250, 253, 254, 255
     };
 
-// initialize random seed
+// initialize random number seed
     srand(*((unsigned int*)aucPassword) % RAND_MAX);
 
-// begin to deal with plaintext data or cipher data
-    for(unsigned long j = 0; j < ulFileSize; j += 256)
+    struct stat statFileSize;
+
+    stat(argv[1], &statFileSize);
+
+// get plaintext or ciphertext file size
+    unsigned long ulFileSize = statFileSize.st_size;
+
+// allocate storage space
+    unsigned char *pucPlaintextOrCiphertext = (unsigned char*)malloc(ulFileSize);
+
+// open plaintext or ciphertext file
+    int iPlaintextOrCiphertext = open(argv[1], O_RDONLY, S_IRUSR | S_IWUSR);
+
+// read data from plaintext or ciphertext file
+    read(iPlaintextOrCiphertext, pucPlaintextOrCiphertext, ulFileSize);
+
+    close(iPlaintextOrCiphertext);
+
+// begin to deal with plaintext or ciphertext data
+    for(unsigned long i = 0; i < ulFileSize; i += 256)
     {
-        unsigned long k, l, ulKeyTableIndex;
+        unsigned long j, k;
 
 // use key table index to change the password 
-        for(k = 0; k < ulPasswordLength; ++k)
-        {
-            ulKeyTableIndex = aucPassword[k] % 256;
-
-            aucPassword[k] = aucKeyTable[ulKeyTableIndex];
-        }
+        for(j = 0; j < ulPasswordLength; ++j) aucPassword[j] = aucKeyTable[aucPassword[j]];
 
 // key table converts 8 * 32 = 256 bytes of data at a time
-        for(l = 0; l < 32; ++l)
+        for(k = 0; k < 32; ++k)
         {
-            unsigned long *pulKeySwap1 = (unsigned long*)aucKeyTable, *pulKeySwap2 = (unsigned long*)aucKeyTable, ulKeyTemp;
+            unsigned long ulKeyIndex, ulKeyTemp, *pulKeySwap1 = (unsigned long*)aucKeyTable, *pulKeySwap2 = (unsigned long*)aucKeyTable;
 
-            if(!(j / 256 % 2))
+            if(!(i / 256 % 2))
             {
-                ulKeyTableIndex = aucPassword[l % ulPasswordLength] % 32;
+                ulKeyIndex = aucPassword[k % ulPasswordLength] % 32;
             }
             else
             {
-                ulKeyTableIndex = rand() % 32;
+                ulKeyIndex = rand() % 32;
             }
 
-            ulKeyTemp = pulKeySwap1[l];
+            ulKeyTemp = pulKeySwap1[k];
 
-            pulKeySwap1[l] = pulKeySwap2[ulKeyTableIndex];
+            pulKeySwap1[k] = pulKeySwap2[ulKeyIndex];
 
-            pulKeySwap2[ulKeyTableIndex] = ulKeyTemp;
+            pulKeySwap2[ulKeyIndex] = ulKeyTemp;
         }
 
-// plaintext data or cipher data processes 256 bytes with XOR at a time
-        for(k = 0; k < 256 && j + k < ulFileSize; ++k)
+// plaintext or ciphertext data processes 256 bytes with XOR at a time
+        for(j = 0; j < 256 && i + j < ulFileSize; ++j)
         {
-               pucPlaintextOrCiphertext[j + k] ^= aucKeyTable[k];
+               pucPlaintextOrCiphertext[i + j] ^= aucKeyTable[j];
         }
     }
 
-// open ciphertext file descriptor
-    int iCiphertextFD = open(argv[2], O_CREAT | O_WRONLY, S_IREAD | S_IWRITE);
+// open ciphertext or plaintext file
+    iPlaintextOrCiphertext = open(argv[2], O_CREAT | O_WRONLY, S_IREAD | S_IWRITE);
 
-// write data to ciphertext file
-    write(iCiphertextFD, pucPlaintextOrCiphertext, ulFileSize);
+// write data to ciphertext or plaintext file
+    write(iPlaintextOrCiphertext, pucPlaintextOrCiphertext, ulFileSize);
 
-    close(iCiphertextFD);
+    close(iPlaintextOrCiphertext);
 
     free(pucPlaintextOrCiphertext);
 
